f20c2f5fc915ae1d61398562926677b8a5557019
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-31 20:06:59 +0530

    added the logic to get the message and calbmi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ function App() {
   const [height, setHeight] = useState(0);
   const [bmi, setBmi] = useState('');
   const [message, setMessage] = useState('')
+  let calcBmi = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+  
 
   return (
     <div className="App">
